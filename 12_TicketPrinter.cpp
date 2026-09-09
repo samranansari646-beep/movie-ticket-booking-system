a63841daf128362c22f0fb7a9967fa916bc4f9a1
samranansari646-beep <samranansari646@gmail.com>
2026-09-09 08:51:26 +0530

    Add TicketPrinter class

--- 12_TicketPrinter.cpp
+++ 12_TicketPrinter.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
 
+class TicketPrinter {
+public:
+    void printTicket(int bookingId, int seatNumber, double price) {
+        cout << "----- MOVIE TICKET -----" << endl;
+        cout << "Booking ID: " << bookingId << endl;
+        cout << "Seat Number: " << seatNumber << endl;
+        cout << "Price: " << price << endl;
+        cout << "------------------------" << endl;
+    }
+};
